0ee7f9ada3bddc8223e4760f0652b9a2fe22efe7
colin <colin-bao@github.com>
2022-04-15 20:52:26 +0800

    Update

--- KMV_Model.py
+++ KMV_Model.py
@@ -1,5 +1,6 @@
 import BSM_Model
 from scipy.stats import norm
+from math import log, sqrt, pi, exp
 
 
 class KMV(BSM_Model.BSM):
@@ -9,9 +10,12 @@ class KMV(BSM_Model.BSM):
     Share_Amount = 0.0
 
     # 计算的参数
+    Debt_Total = 0.0
     Share_MV = 0.0
     DD = 0.0
     PD = 0.0
+    d1_KMV = 0.0
+    d2_KMV = 0.0
 
     # 从BSM父类继承方法和属性#
     def __init__(self, K, share_name, date_dict, Debt_Short, Debt_Long, Share_Amount):
@@ -19,21 +23,32 @@ class KMV(BSM_Model.BSM):
         self.Debt_Short, self.Debt_Long, self.Share_Amount = Debt_Short, Debt_Long, Share_Amount
 
         # 计算股权价值
-        self.Share_MV = self.Share_Amount * self.S
+        self.Share_MV = Share_Amount * self.S
+        # 计算债权价值
+        self.Debt_Total = Debt_Short + 0.5 * Debt_Long
 
         # 计算新的DD
-        self.DD = self.cal_DD
+        self.DD = self.cal_DD()
 
         # 计算PD
-        self.PD = self.cal_PD
+        self.PD = self.cal_PD()
 
+    def cal_d1_KMV(self, S, K, T, r, sigma):
+        self.d1_KMV = (log(S / K) + (r + sigma ** 2 / 2.0) * T) / (sigma * sqrt(T))
+        return self.d1_KMV
+
+    def cal_d2_KMV(self, S, K, T, r, sigma):
+        self.d2_KMV = self.cal_d1_KMV(S, K, T, r, sigma) - sigma * sqrt(T)
+        return self.d2_KMV
+
+    # 把股权价值换成股价，债务价值换成K
     def cal_DD(self):
-        return self.d2
+        return self.cal_d2_KMV(self.Share_MV, self.Debt_Total, self.T, self.r, self.sigma)
 
     def cal_PD(self):
-        return norm.cdf(-self.d2)
+        return norm.cdf(-self.d2_KMV)
 
 
-KMV_new = KMV(100, '000001.sz', {'start_date': '20200101', 'end_date': '20210101', 'expiry_day': '20220101'},
+KMV_new = KMV(100, '000651.sz', {'start_date': '20200101', 'end_date': '20210101', 'expiry_day': '20220101'},
               154527413000, 3354563900, 6015730900)
-print(KMV_new.bs_call, KMV_new.bs_put, KMV_new.DD)
+print(KMV_new.bs_call, KMV_new.bs_put, KMV_new.DD, KMV_new.PD, KMV_new.S, KMV_new.Share_MV)
