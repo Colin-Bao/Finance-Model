b1361d49494f1cf6fb05bd85dadbf6a26070352f
colin <colin-bao@github.com>
2022-04-16 10:49:08 +0800

    Update

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import BSM_Model
 import KMV_Model
 
 if __name__ == '__main__':
+    BSM_new = BSM_Model.BSM(100, '000651.sz',
+                            {'start_date': '20200101', 'end_date': '20210101', 'expiry_day': '20220101'})
+    print(BSM_new.bs_call)
     KMV_new = KMV_Model.KMV(100, '000651.sz',
                             {'start_date': '20200101', 'end_date': '20210101', 'expiry_day': '20220101'},
                             154527413000, 3354563900, 6015730900)
