bdeddda5cb94df3292ab3d00fbb64884e9bbe303
colin <colin-bao@github.com>
2022-05-02 17:24:48 +0800

    Update

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import BSM_Model
 
-# if __name__ == '__main__':
-# 用K，S，t完成初始化
 # %%
+# 用K，S，t完成初始化
 BSM = BSM_Model.BSM(60, '000651.sz',
                     {'start_date': '20200101', 'end_date': '20210101', 'expiry_day': '20210101'})
 # 可视化
